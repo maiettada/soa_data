07fa42382062ca3a1078436669ecd97e7837c8da
Davide Maietta <maiettada@gmail.com>
2021-03-04 17:53:29 +0100

    training.Example instead of GoldParse class

---
+++ README.md
@@ -0,0 +1,1 @@
+# soa_data_scorer
---
+++ main.py
@@ -0,0 +1,27 @@
+import spacy
+from spacy.training import Example
+from spacy.scorer import Scorer
+
+
+def evaluate(ner_model, examples):
+    scorer = Scorer(ner_model)
+    for input_, annot in examples:
+        doc_gold_text = ner_model.make_doc(input_)
+        gold = Example.from_dict(doc_gold_text, {"entities": annot})
+        pred_value = ner_model(input_)
+        scorer.score(gold)
+    return scorer.scores
+
+# example run
+
+examples = [
+    ('Who is Shaka Khan?',
+     [(7, 17, 'PERSON')]),
+    ('I like London and Berlin.',
+     [(7, 13, 'LOC'), (18, 24, 'LOC')])
+]
+
+#ner_model = spacy.load(ner_model_path) # for spaCy's pretrained use 'en_core_web_sm'
+
+ner_model = spacy.load('en_core_web_sm')
+results = evaluate(ner_model, examples)
