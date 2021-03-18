f5390e9980dc0134ddb4afbb48f1a89042817391
Davide Maietta <maiettada@gmail.com>
2021-03-18 19:05:50 +0100

    add TRAINING_DATA set

--- example.py
+++ example.py
@@ -18,11 +18,19 @@ patterns = [
 ]
 matcher.add("TEST_PATTERNS", patterns, on_match=on_match)
 
-
-# Add patterns to the matcher and check the result
+TRAINING_DATA = []
+# Create a Doc object for each text in TEXTS
 for doc in nlp.pipe(TEXTS):
-    print([doc[start:end] for match_id, start, end in matcher(doc)])
+    # Match on the doc and create a list of matched spans
+    spans = [doc[start:end] for match_id, start, end in matcher(doc)]
+    # Get (start character, end character, label) tuples of matches
+    entities = [(span.start_char, span.end_char, "GADGET") for span in spans]
+    # Format the matches as a (doc.text, entities) tuple
+    training_example = (doc.text, {"entities": entities})
+    # Append the example to the training data
+    TRAINING_DATA.append(training_example)
 
+print(*TRAINING_DATA, sep="\n")
 
 '''
 nlp.pipe(VETTORE_DI_TESTI) 
