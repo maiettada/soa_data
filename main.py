c7fac542ef9c3a7b80920ef3308ad8ee6deee4fd
Davide Maietta <maiettada@gmail.com>
2021-05-10 13:10:47 +0200

    compare unsorted annotation_lists
    -compares gold-annotations and labelled-annotations if meta.ord_id corresponds
    -prevent errors for missing order_id-s (assuming empty annotation_list)

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-import spacy
-from spacy.training import Example
-from spacy.scorer import Scorer
-import pickle
 import json
+import pickle
+
+import spacy
+from spacy.scorer import Scorer
+from spacy.training import Example
 
 gold_obj_json_string = '[\
-    {"text": "I like Europe and ice-creams.","meta": {"ord_id": 91, "fr_id": 0}, "labels": [[7, 13, "GPE"],[18,28,"food"]]},\
-    {"text": "I like Europe and ice-creams.","meta": {"ord_id": 91, "fr_id": 0}, "labels": [[7, 13, "GPE"],[18,28,"food"]]},\
-    {"text": "I like Europe and ice-creams.","meta": {"ord_id": 91, "fr_id": 0}, "labels": [[7, 13, "GPE"],[18,28,"food"]]}\
+    {"text": "I like Europe and ice-creams.","meta": {"ord_id": 1, "fr_id": 0}, "labels": [[7, 13, "GPE"],[18,28,"food"]]},\
+    {"text": "I like Europe and ice-creams.","meta": {"ord_id": 2, "fr_id": 0}, "labels": [[7, 13, "GPE"],[18,28,"food"]]},\
+    {"text": "I like Europe and ice-creams.","meta": {"ord_id": 4, "fr_id": 0}, "labels": [[7, 13, "GPE"],[18,28,"food"]]}\
 ]'
 
 labelled_obj_json_string = '[\
-    {"meta": {"ord_id": 91, "fr_id": 0}, "labels": [[7, 13, "GPE"],[18,28,"food"]]},\
-    {"meta": {"ord_id": 91, "fr_id": 0}, "labels": [[7, 13, "GPE"]]},\
-    {"meta": {"ord_id": 91, "fr_id": 0}, "labels": [[7, 13, "GPE"]]}\
+    {"meta": {"ord_id": 1, "fr_id": 0}, "labels": [[7, 13, "GPE"],[18,28,"food"]]},\
+    {"meta": {"ord_id": 2, "fr_id": 0}, "labels": [[7, 13, "GPE"]]},\
+    {"meta": {"ord_id": 3, "fr_id": 0}, "labels": [[7, 13, "GPE"]]}\
 ]'
 
 
@@ -25,7 +26,7 @@ def load_json_line_gold(gold_obj_json):
 
 def format_json_line_data_gold(gold_obj):
     '''handling just json-data-structures'''
-    loaded_gold_data = [[item.get('text'), item.get('labels')] for item in gold_obj]
+    loaded_gold_data = [[item.get('text'), item.get('labels'), item.get('meta').get('ord_id')] for item in gold_obj]
     return loaded_gold_data
 
 
@@ -37,7 +38,7 @@ def load_json_line_labelled(labelled_obj_json):
 
 def format_json_line_data_labelled(labelled_obj):
     '''handling just json-data-structures'''
-    loaded_labelled_data = [item.get('labels') for item in labelled_obj]
+    loaded_labelled_data = [[item.get('labels'), item.get('meta').get('ord_id')] for item in labelled_obj]
     return loaded_labelled_data
 
 
@@ -67,21 +68,24 @@ def init_nlp():
     return nlp
 
 
-def evaluate(ner_model, gold_annotations, labelled_data_list):
+def evaluate(ner_model, gold_annotations, labelled_data_lines):
     scorer = Scorer(ner_model)
     list = []
     i = 0
-    for input_textunit_, gold_annot in gold_annotations:
-        if labelled_data_list:
-            label_list_i = labelled_data_list[i]
-            if i + 1 < len(labelled_data_list):
-                i = i + 1
-        else:
+    for [gold_textunit, gold_annots, gold_ord_id] in gold_annotations:
+        if not labelled_data_lines:
             label_list_i = []
-        labelled_ner_textunit = ner_model.make_doc(input_textunit_)
-        spans = [ labelled_ner_textunit.char_span(label[0], label[1], label[2]) for label in label_list_i]
+        else:
+            label_list_i_enclosed = [labelled_data_line_objects[0] for labelled_data_line_objects in labelled_data_lines
+                            if labelled_data_line_objects[1] == gold_ord_id]
+            if label_list_i_enclosed:
+                label_list_i = label_list_i_enclosed[0]
+            else:
+                label_list_i = []
+        labelled_ner_textunit = ner_model.make_doc(gold_textunit)
+        spans = [ labelled_ner_textunit.char_span(start_offset, end_offset, word) for [start_offset, end_offset, word] in label_list_i ]
         labelled_ner_textunit.ents = spans
-        item = Example.from_dict(labelled_ner_textunit, {"entities": gold_annot})
+        item = Example.from_dict(labelled_ner_textunit, {"entities": gold_annots})
         list.append(item)
     scores = scorer.score(list)
     return scores
@@ -99,6 +103,8 @@ def load_pickle_data():
 
 def load_from_file():
     #return load_pickle_data()
+    # json line: it's a full JSON  of a jsonl file.
+    # In this context, a json line is related to an entire document
     return [load_json_line_gold(gold_obj_json_string), load_json_line_labelled(labelled_obj_json_string)]
 
 
@@ -126,3 +132,11 @@ file_data = load_from_file()
 print(ner_model.pipe_names)
 results = evaluate(ner_model, loaded_gold_data, loaded_labelled_data)
 print(results)
+
+
+
+
+
+
+
+
