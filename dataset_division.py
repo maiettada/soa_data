8e66e50ea03bdf4bac7104e9ad0d39ba5b880703
Davide Maietta <maiettada@gmail.com>
2021-06-24 21:18:24 +0200

    progressively divide a document in periods

---
+++ dataset_division.py
@@ -0,0 +1,48 @@
+def sort_list(lis):
+ lis.sort(key=lambda x:x[0])
+
+def prendo_frase_fino_a(document, char_index):
+    selection = ""
+    remaining = ""
+    if char_index == -1:
+        selection = document
+        remaining = None
+    else:
+        selection = document[:char_index]
+        remaining = document[char_index+2:]
+    return [selection, remaining]
+
+def prendo_labels_fino_a(json_list, char_index):
+    selection = []
+    remaining = []
+    if char_index == -1:
+        selection = json_list
+        remaining = None
+    else:
+        selection = [ [st,end,word] for [st,end,word] in json_list if end <= char_index ]
+        remaining = [ [st - char_index - 2,end - char_index - 2,word] for [st,end,word] in json_list if end > char_index ]
+    return [selection, remaining]
+
+def recursive_select(json_list, txt):
+    # = int(input('Enter a number: '))
+    sort_list(json_list)
+    x = txt.find(".")
+    json_sublist = []
+    [ frase, documento ] = prendo_frase_fino_a( txt, x )
+    [json_sublist, json_list] = prendo_labels_fino_a(json_list, x)
+    '''print(x) #char index
+    print(frase)
+    print(json_sublist)
+    print(documento)
+    print(json_list)'''
+    if x !=-1:
+        a.append([frase, json_sublist])
+        recursive_select(json_list, documento)
+    return
+
+a=[]
+txt = "Hello. Welcome. Sit down. "
+json_list = [[0,5,"p1"],[7,14,"p2"],[16,19,"p3w1"],[20,24,"p3w2"]]
+recursive_select(json_list, txt)
+print("-----")
+print(a)
