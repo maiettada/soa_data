61f274e37e57fef21eaef7b9eac98f4ea5d791e6
Davide Maietta <maiettada@gmail.com>
2021-06-26 16:36:45 +0200

    implement automata

--- dataset_division.py
+++ dataset_division.py
@@ -1,3 +1,22 @@
+from automata.fa.dfa import DFA
+# DFA which matches all binary strings ending in an odd number of '1's
+dfa = DFA(
+    states={'q0', 'q1'},
+    input_symbols={'0', '1'},
+    transitions={
+        'q0': {'0': 'q0', '1': 'q1'},
+        'q1': {'0': 'q0', '1': 'q0'}
+    },
+    initial_state='q0',
+    final_states={'q1','q0'}
+)
+
+
+print(dfa.read_input('0011'))
+print(dfa.read_input('00111'))
+print(dfa.read_input('001111'))
+
+
 class labelledObj:
 
     # default constructor
@@ -83,3 +102,5 @@ for label in label_list:
 for obj in obj_list:
     obj.print_label()
     obj.print_list()
+
+
