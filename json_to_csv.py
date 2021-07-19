d1f23b50b04db4609478e5ad10edc960f42b15fe
Davide Maietta <maiettada@gmail.com>
2021-07-19 17:32:30 +0200

    print csv output in the specific soa order

--- json_to_csv.py
+++ json_to_csv.py
@@ -4,16 +4,15 @@ import pandas as pd
 
 pd.__version__
 
-label_subset_os_og_categories = soa_categorie
-label_subset_classes = soa_classifiche
+labels = soa_categorie + soa_classifiche
 
 
 def fill_columns(labels, col_1, col_2, col_3):
     df = pd.DataFrame({
                        'Labels': labels,
-                       'Colonna 1': col_1,
-                       'colonna 2': col_2,
-                       'colonna 3': col_3
+                       'Regex P': col_1,
+                       'Regex R': col_2,
+                       'Regex F': col_3
                        })
     return df
 
@@ -22,28 +21,35 @@ def write_df(filepath, df):
     df.to_csv(filepath)
 
 
-def write_to_csv(filepath, list_items, label_subset_classes, df):
+def write_to_csv(filepath, df):
     write_df(filepath, df)
     return
 
+def make_ordered_list(entities, ordering_list):
+    ordered_list = []
+    for e in ordering_list:
+        if e in ents_per_type:
+            ordered_list.append([e, ents_per_type[e]['p'], ents_per_type[e]['r'], ents_per_type[e]['f']])
+        else:
+            ordered_list.append([e, "None", "None", "None"])
+    return ordered_list
 
 with open('json-results/result1-regex-soa-categories.json', 'r') as fp:
     line = fp.readline()
     json_elements = json.loads(line) #load-string line
     ents_per_type = json_elements['ents_per_type']
-    keys=[x for x in ents_per_type.keys()]
-    #values = [x for x in ents_per_type.values()]
-    values_p = [x['p'] for x in ents_per_type.values()]
-    values_r = [x['r'] for x in ents_per_type.values()]
-    values_f = [x['f'] for x in ents_per_type.values()]
+    ordered = make_ordered_list(ents_per_type, soa_categorie_valide)
 with open('json-results/result1-regex-soa-classification.json', 'r') as fp:
     line = fp.readline()
     json_elements = json.loads(line) #load-string line
     ents_per_type = json_elements['ents_per_type']
-    keys = keys + [x for x in ents_per_type.keys()]
-    #values = [x for x in ents_per_type.values()]
-    values_p = values_p + [x['p'] for x in ents_per_type.values()]
-    values_r = values_r + [x['r'] for x in ents_per_type.values()]
-    values_f = values_f + [x['f'] for x in ents_per_type.values()]
+    ordered = ordered + make_ordered_list(ents_per_type, soa_classifiche)
+    keys =  [x[0] for x in ordered ]
+    values_p = [x[1] for x in ordered]
+    values_r = [x[2] for x in ordered]
+    values_f = [x[3] for x in ordered]
     df = fill_columns(keys, values_p, values_r, values_f )
-    write_to_csv('soa_os_og.csv', list(json_elements.keys()), label_subset_os_og_categories, df)
+    write_to_csv('soa_os_og_regex.csv', df)
+    write_to_csv('soa_os_og_full_automa.csv', df)
+    write_to_csv('soa_os_og_full_rnd.csv', df)
+    write_to_csv('soa_os_og_partial.csv', df)
--- soa_data.py
+++ soa_data.py
@@ -1,11 +1,15 @@
 from typing import List
 
+
 soa_classifiche: List[str] = ['I', 'II', 'III-bis', 'IV', 'IV-bis', 'V', 'VI', 'VII', 'VIII']
 
 
-soa_categorie: List[str] = ['OG-1', 'OG-2', 'OG-3', 'OG-4', 'OG-5', 'OG-6', 'OG-7', 'OG-8', 'OG-9', 'OG-10',
+
+soa_categorie_valide: List[str] = ['OG-1', 'OG-2', 'OG-3', 'OG-4', 'OG-5', 'OG-6', 'OG-7', 'OG-8', 'OG-9', 'OG-10',
                  'OG-11', 'OG-12', 'OG-13', 'OS-1', 'OS-2A', 'OS-2B', 'OS-3', 'OS-4', 'OS-5', 'OS-6',
                  'OS-7', 'OS-8', 'OS-9', 'OS-10', 'OS-11', 'OS-12A', 'OS-12B', 'OS-13',
                  'OS-14', 'OS-15', 'OS-16', 'OS-17', 'OS-18A', 'OS-18B', 'OS-19', 'OS-20A',
                  'OS-20B', 'OS-21', 'OS-22', 'OS-23', 'OS-24', 'OS-25', 'OS-26', 'OS-27',
-                 'OS-28', 'OS-29', 'OS-30', 'OS-31', 'OS-32', 'OS-33', 'OS-34', 'OS-35', '']
+                 'OS-28', 'OS-29', 'OS-30', 'OS-31', 'OS-32', 'OS-33', 'OS-34', 'OS-35']
+
+soa_categorie: List[str] = soa_categorie_valide + ['']
