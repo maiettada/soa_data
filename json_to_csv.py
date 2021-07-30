db985cd7fcade9333998d3228a3260b25ee41efe
Davide Maietta <maiettada@gmail.com>
2021-07-30 16:52:30 +0200

    reformat json-to-csv file

--- json_to_csv.py
+++ json_to_csv.py
@@ -14,11 +14,11 @@ def fill_columns(labels, col_1, col_2, col_3):
     :rtype: pandas dataframe built with the columns data
     """
     df = pd.DataFrame({
-                       'Labels': labels,
-                       'Regex P': col_1,
-                       'Regex R': col_2,
-                       'Regex F': col_3
-                       })
+        'Labels': labels,
+        'Regex P': col_1,
+        'Regex R': col_2,
+        'Regex F': col_3
+    })
     return df
 
 
@@ -83,20 +83,20 @@ to be merged
 """
 with open('json-results/result1-regex-soa-categories.json', 'r') as fp:
     line = fp.readline()
-    json_elements = json.loads(line) #load-string line
+    json_elements = json.loads(line)  # load-string line
     ents_per_type = json_elements['ents_per_type']
     ordered = make_ordered_list(ents_per_type, soa_categorie_valide)
 with open('json-results/result1-regex-soa-classification.json', 'r') as fp:
     line = fp.readline()
-    json_elements = json.loads(line) #load-string line
+    json_elements = json.loads(line)  # load-string line
     ents_per_type = json_elements['ents_per_type']
     ordered = ordered + make_ordered_list(ents_per_type, soa_classifiche)
-    keys =  [x[0] for x in ordered ]
+    keys = [x[0] for x in ordered]
     values_p = [x[1] for x in ordered]
     values_r = [x[2] for x in ordered]
     values_f = [x[3] for x in ordered]
-    df = fill_columns(keys, values_p, values_r, values_f )
+    df = fill_columns(keys, values_p, values_r, values_f)
     write_to_csv('csv_tables/soa_os_og_regex.csv', df)
-json_results_to_csv('json-results/result2-full-automa.json','csv_tables/soa_os_og_full_automa.csv')
-json_results_to_csv('json-results/result3-full-rnd.json','csv_tables/soa_os_og_full_rnd.csv')
-json_results_to_csv('json-results/result4-partial-sentences.json','csv_tables/soa_os_og_partial.csv')
+json_results_to_csv('json-results/result2-full-automa.json', 'csv_tables/soa_os_og_full_automa.csv')
+json_results_to_csv('json-results/result3-full-rnd.json', 'csv_tables/soa_os_og_full_rnd.csv')
+json_results_to_csv('json-results/result4-partial-sentences.json', 'csv_tables/soa_os_og_partial.csv')
